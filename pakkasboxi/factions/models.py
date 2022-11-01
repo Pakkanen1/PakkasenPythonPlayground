87a4cfdf56a6e7187379f09f230325726a3a1b56
Pakkanen1 <bonetailur@gmail.com>
2022-11-01 19:21:44 +0200

    factiontofaction taulut

--- pakkasboxi/factions/models.py
+++ pakkasboxi/factions/models.py
@@ -29,21 +29,31 @@ class Character(SurrogatePK, Model):
         db.Model.__init__(self, name=name, description=description, active=active, npc=npc, **kwargs)
 
 
-class FactionReputation(SurrogatePK, Model):
+class CharacterToFactionReputation(SurrogatePK, Model):
 
-    __tablename__ = "factionreputations"
-    faction_id = db.Column(db.Integer, nullable=False)  # faction that has the reputation
-    faction = relationship("Faction", backref=db.backref("factions"))
-    related_faction_id = reference_col("factions", nullable=True)  # the faction that has given the reputation
-    related_faction = relationship("Faction", backref=db.backref("factions"))
-    character_id = db.Column(db.Integer, nullable=True)
+    __tablename__ = "charactertofactionreputations"
+    character_id = reference_col("characters", nullable=False)
     character = relationship("Character", backref=db.backref("characters"))
+    faction_id = reference_col("factions", nullable=False)  # faction that gives the reputation
+    faction = relationship("Faction", backref=db.backref("factions"))
     reputation_points = db.Column(db.Integer, nullable=False)
     created_ts = Column(db.DateTime, nullable=False, default=dt.datetime.now)
     modified_ts = Column(db.DateTime, nullable=False, default=dt.datetime.now)
 
-    def __init__(self, faction, related_faction, character, reputation_points, **kwargs):
-        db.Model.__init__(self, faction=faction, related_faction=related_faction,
-                          character=character, reputation_points=reputation_points, **kwargs)
+    def __init__(self, faction, character, reputation_points, **kwargs):
+        db.Model.__init__(self, faction=faction, character=character, reputation_points=reputation_points, **kwargs)
 
+class FactionToFactionReputation(SurrogatePK, Model):
 
+    __tablename__ = "factiontofactionreputations"
+    faction_id = reference_col("factions", nullable=False)  # faction that has the reputation
+    faction = relationship("Faction", backref=db.backref("factions"))
+    target_faction_id = reference_col("factions", nullable=True)  # the faction that has given the reputation
+    target_faction = relationship("Faction", backref=db.backref("factions"))
+    reputation_points = db.Column(db.Integer, nullable=False)
+    created_ts = Column(db.DateTime, nullable=False, default=dt.datetime.now)
+    modified_ts = Column(db.DateTime, nullable=False, default=dt.datetime.now)
+
+    def __init__(self, faction, target_faction, reputation_points, **kwargs):
+        db.Model.__init__(self, faction=faction, target_faction=target_faction,
+                          reputation_points=reputation_points, **kwargs)
